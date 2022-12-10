220b3f8c76d81a3b38a2e50d955adf5f580c38fd
dannyfwalter1 <wal16025@byui.edu>
2022-12-09 21:45:52 -0700

    adding folders

---
+++ invaders/game/casting/ship.py
@@ -0,0 +1,39 @@
+import constants
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+
+class Ship(Actor):
+    """
+    A long limbless reptile.
+    
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
+    def __init__(self):
+        super().__init__()
+        self._segments = []
+        self._prepare_body()
+
+    def get_segments(self):
+        return self._segments
+
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
+    def get_head(self):
+        return self._segments[0]
+    
+    def _prepare_body(self):
+        x = int(constants.MAX_X / 2)
+        y = int(constants.MAX_Y)
